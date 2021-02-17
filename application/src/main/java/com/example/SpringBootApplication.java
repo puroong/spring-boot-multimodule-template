3d5deb34b771d0479fd4b08a3aaa3f735af6c589
nostalgia <skfk0135@gmail.com>
2021-02-17 06:40:27 +0000

    initial commit

---
+++ application/src/main/java/com/example/SpringBootApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
